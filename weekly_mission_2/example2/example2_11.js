6dca671036285d1834649cbdd1ad7b0ac269cf61
davidjuarezr <djuarezr@gmail.com>
2022-04-29 05:41:54 -0500

    Agregando metodos a mi clase

--- weekly_mission_2/example2/example2_11.js
+++ weekly_mission_2/example2/example2_11.js
@@ -10,6 +10,13 @@ const woopa_obj = {
 
 //azucar sintactica  para abstraer
 //Crear una clase
+/*
+1. como crear una clase
+2. como instanciar un objeto de una clase
+3. instanciar varios objetos de una misma clase
+4. como agregar el constructor para guardar atributos al crear un objeto
+5. crear métodos - funciones  delimitar comportamiento de la información
+*/
 class Ajolonauta{
     constructor(name, mission, age,color){
         this.name = name
@@ -18,6 +25,10 @@ class Ajolonauta{
         this.color = color
     }   //instanciar los objetos que se crean de esta clase
     //.this hace referencia a las propiedades del objeto
+    sayHello(){
+        // return "Hey! Hello"
+        return `${this.name} is saying Hello!`
+    }
 } //es el molde
 
 //objeto de JS
@@ -27,10 +38,11 @@ console.log(woopa_obj_js)
 //se crea un objeto del molde
 const woopa =  new Ajolonauta("Woopa", "Node JS", 18, "Pink") //se instancia un objeto vacio
 console.log(woopa)
-
+console.log(woopa.sayHello())
 
 const wooper =  new Ajolonauta("Wooper","java", 53, "blue") //se hace
-console.log(wooper)
+console.log(wooper.name)
+console.log(wooper.sayHello())
 
 // const woopy =  new Ajolonauta() //orientacion a objetos
 // console.log(woopy)
