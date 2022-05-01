01498a8528d1c91c6c9d05a186d3dcd2bce554e5
davidjuarezr <djuarezr@gmail.com>
2022-05-01 00:56:58 -0500

    Paso 7: Agregando requerimientos 2 (modificaciones)

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,13 +1,19 @@
 const User = require('./../../app/models/User')
 
-describe("Prueba de jest", () => {
-    test("prueba 1", () => {
-        const user = new User(1, "JorgeDavid", "Jorge", "Bio", "dateCreated", "lastUpdate")
+describe("Test para User", () => {
+    test("Requerimiento 1: Creación de User", () => {
+        const user = new User(1, "JorgeDavid", "Jorge", "Bio") //, "dateCreated", "lastUpdate"
         expect(user.id).toBe(1)
         expect(user.username).toBe("JorgeDavid")
         expect(user.name).toBe("Jorge")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")git 
-        expect(user.lastUpdated).toBe("lastUpdated")
+        // expect(user.dateCreated).toBe("dateCreated")
+        // expect(user.lastUpdated).toBe("lastUpdated")
+    })
+
+    test("Requerimiento 2: Fechas en atributos de User", () => {
+        const user = new User(1, "jorgedavid", "jorge", "Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
