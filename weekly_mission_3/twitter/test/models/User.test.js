f013da39c817841dfdda4586ea418d53f5510917
davidjuarezr <djuarezr@gmail.com>
2022-05-01 00:20:54 -0500

    Paso 6: Agregamos clase User y su respectiva prueba

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -1,6 +1,13 @@
+const User = require('./../../app/models/User')
 
 describe("Prueba de jest", () => {
-    test("prueba 2", () => {
-        expert(10).toBe(9)
+    test("prueba 1", () => {
+        const user = new User(1, "JorgeDavid", "Jorge", "Bio", "dateCreated", "lastUpdate")
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("JorgeDavid")
+        expect(user.name).toBe("Jorge")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")git 
+        expect(user.lastUpdated).toBe("lastUpdated")
     })
 })
