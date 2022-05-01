58a220681afc1ce2dcf9c0465adb4d2575db59ff
davidjuarezr <djuarezr@gmail.com>
2022-05-01 02:10:44 -0500

    Paso 9: Agregando requerimiento 4 y su prueba de unidad

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -18,7 +18,16 @@ describe("Test para User", () => {
     })
 
     test("Requerimiento 3: Agregando getters", () => {
-        const user = new User(1, "jorgedavid", "carlo", "Bio")
+        const user = new User(1, "jorgedavid", "jorge", "Bio")
         expect(user.getUsername).toBe("jorgedavid")
     })
+
+    test("Requerimiento 4: Agregando setters", () => {
+        const user = new User(1, "jorgedavid", "jorge", "Bio")
+        user.setUsername = "jorgedavid"
+        expect(user.username).toBe("jorgedavid")
+
+        // user.setBio = "New bio"
+        // expect(user.bio).toBe("New bio")
+    });
 })
