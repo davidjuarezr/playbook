4aa0819eb181f0c9b3afdad7b6d484f1cff4a739
davidjuarezr <djuarezr@gmail.com>
2022-05-02 02:32:35 -0500

    Paso 3: Agregando mi primer app con express

---
+++ weekly_mission_3/express_basic/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000  //localhost:3000
+
+//localhost:3000
+app.get('/', (req, res) => {
+    res.send("Hola a todos mis explorers")
+})
+
+app.listen(port, () => {
+    console.log("Server Listo!")
+})
+
