1199d5a419d791196d5681b0732d9ff137480268
davidjuarezr <djuarezr@gmail.com>
2022-05-04 06:33:31 -0500

    Paso 1: Agregando endpoint GET /v1/explorers

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -15,9 +15,22 @@ app.get('/v1/explorers', (req, res) => {
     const explorers = [explorer1, explorer2]
     //HTTP CODE STATUS: 200
     res.status(200).json(explorers)
- 
 })
 
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`GET BY ID Explorers V1 API ${new Date()}`)
+    const explorer1 = {id: 1, name: "Explorer1"}
+    //HTTP CODE STATUS: 200
+    res.status(200).json(explorer1)
+})
+
+app.post('/v1/explorers/', (req, res) => {
+    console.log(`POST Explorers V1 API ${new Date()}`)
+    // Agregar la lógica para persisitir
+    console.log(req.body) //parámetros del request    
+    //HTTP CODE STATUS: 200
+    res.status(201).json({ message: "Creado exitosamente"})
+})
 
 // Con esto iniciamos esta app
 app.listen(port, () => {
